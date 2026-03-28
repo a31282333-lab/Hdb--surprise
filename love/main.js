fd831251df9baced2be375b7dff4fdc9852be071
a31282333-lab <a31282333@gmail.com>
2026-03-28 18:51:42 +0530

    Create main.js

---
+++ love/main.js
@@ -0,0 +1,15 @@
+const name = "BISHIDA"; 
+const messages = [
+    "Hey Birthday BABY! ✨",
+    "Aaj ka din sirf tumhara hai...",
+    "Class 8 ki Result ki tension chodo aur smile karo!",
+    "Tum meri life ki sabse beautiful variable ho ❤️",
+    "Har pal tumhare saath special hota hai 😊",
+    "Ye chota sa surprise tumhare liye...",
+    "Hamesha aise hi khush rehna!",
+    "Keep shining like a star 🌟",
+    "I Love You So Much!",
+    "HAPPY BIRTHDAY! 🎂😋💝🌼"
+];
+
+// ... (Baaki animation wala code yahan aayega)
